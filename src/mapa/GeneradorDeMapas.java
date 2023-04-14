9d4cba91c3469a9932c803fd2ed20e6cd3548505
JereDAM <usr3593@salesianos-lacuesta.net>
2023-04-14 12:51:57 +0100

    Created branch Feature

--- src/mapa/GeneradorDeMapas.java
+++ src/mapa/GeneradorDeMapas.java
@@ -3,11 +3,13 @@ import java.util.Arrays;
 
 public class GeneradorDeMapas {
     
-   
    public static String[][] dibujarMapa(int tamanioMapa){
    
        String [][] dibujoMapa = new String[tamanioMapa][tamanioMapa];
 
+       for (int i = 0; i < dibujoMapa.length; i++) {
+            
+       }
        return dibujoMapa;
     }
     
--- src/mapa/Mapa.java
+++ src/mapa/Mapa.java
@@ -3,11 +3,10 @@ package mapa;
 public class Mapa {
     
     private int tamanioMapa;
-    private int mapaGenerado;
+    private String[][] mapaGenerado;
 
     public Mapa(int tamanioMapa){
         this.tamanioMapa = tamanioMapa;
+        this.mapaGenerado = GeneradorDeMapas.dibujarMapa(tamanioMapa);
     }
-
-    
 }
